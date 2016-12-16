e2d9a6917ca6a1266ffff66a7a98491ec263ebe1
psycura <psycura@gmail.com>
2016-12-16 19:02:30 +0200

    cleaned

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,12 @@
 let gulp = require('gulp');
-let cmq = require('gulp-combine-media-queries');
 let autoprefixer = require('gulp-autoprefixer');
-
-let paths = {
-    styles : {
-        src : 'src/css/*/*.css',
-        dist : 'dist/css'
-    },
-    scripts : {
-        src : 'src/js/*.js',
-        dist : 'dist/js'
-    }
-};
 
 gulp.task('styles', function () {
     return gulp.src('src/css/main.css')
-        .pipe(cmq({
-            log : true
-        }))
         .pipe(autoprefixer({
-            browsers : ['last 3 versions']
+            browsers : ['last 2 versions'],
+            cascade: false
         }))
-        
-        .pipe(gulp.dest('dist/css'));
+               
+        .pipe(gulp.dest('dist/css/'));
 });
