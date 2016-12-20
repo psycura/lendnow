e5da3d691f8bd83db6cd4cc906482b9cf7a786c4
Eugene Alitz <psycura@gmail.com>
2016-12-20 17:45:22 +0200

    cleaned

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,58 @@
 let gulp = require('gulp');
 let autoprefixer = require('gulp-autoprefixer');
+let spritesmith = require('gulp.spritesmith');
+let csso = require('gulp-csso');
+
+let SRC_DIR = 'src/';
+let DIST_DIR = 'dist/';
+
+let path = {
+    styles : {
+        src : SRC_DIR + 'css/**/*.css',
+        dist : DIST_DIR + 'css'
+    },
+    scripts : {
+        src : SRC_DIR + 'js/*.js',
+        dist : DIST_DIR + 'js'
+    },
+    assets : {
+        src : SRC_DIR + 'img/**/*.*',
+        dist : DIST_DIR + 'img'
+    },
+    html : {
+        src : SRC_DIR + '**/*.html',
+        dist : DIST_DIR
+    },
+    sprites : {
+        src : SRC_DIR + 'img/icons/*.png',
+        distImg : DIST_DIR + 'img',
+        imgLocation : 'img/sprite.png',
+        distFile : DIST_DIR + 'css/sprite'
+    }
+    
+};
 
 gulp.task('build', ['pages', 'styles', 'scripts', 'assets', 'fonts']);
 
 gulp.task('styles', function () {
-    return gulp.src('src/css/**/*.css')
+    return gulp.src(path.styles.src)
         .pipe(autoprefixer({
-            browsers : ['last 2 versions'],
-            cascade: false
+            browsers : ['>5%'],
+            cascade : false
         }))
-        .pipe(gulp.dest('dist/css/'));
+        .pipe(csso())
+        .pipe(gulp.dest(path.styles.dist));
+    
 });
 
 gulp.task('scripts', function () {
-    return gulp.src('src/js/*.js')
-        .pipe(gulp.dest('dist/js/'));
+    return gulp.src(path.scripts.src)
+        .pipe(gulp.dest(path.scripts.dist));
 });
 
 gulp.task('assets', function () {
-    return gulp.src('src/img/**/*.*')
-        .pipe(gulp.dest('dist/img/'));
+    return gulp.src(path.assets.src)
+        .pipe(gulp.dest(path.assets.dist));
 });
 
 gulp.task('fonts', function () {
@@ -28,7 +61,23 @@ gulp.task('fonts', function () {
 });
 
 gulp.task('pages', function () {
-    return gulp.src('src/**/*.html')
-        .pipe(gulp.dest('dist/'));
+    return gulp.src(path.html.src)
+        .pipe(gulp.dest(path.html.dist));
+});
+
+
+/* ---------- sprite generator ----------*/
+gulp.task('sprite', function () {
+    let spriteData = gulp.src(path.sprites.src)
+        .pipe(spritesmith({
+            imgName : 'sprite.png',
+            cssName : 'sprite.scss',
+            cssFormat:'css',
+            imgPath:path.sprites.imgLocation,
+            padding:70
+        }));
+    spriteData.img.pipe(gulp.dest(path.sprites.distImg));
+    spriteData.css.pipe(gulp.dest(path.sprites.distFile));
+    
 });
 
